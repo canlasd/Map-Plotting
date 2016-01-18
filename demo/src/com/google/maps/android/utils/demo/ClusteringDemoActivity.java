c6a34b461ce03a880d098b534b5ddd48f7f097e6
canlasd <david@getliveapp.com>
2016-01-18 14:12:38 -0500

    initial commit

--- demo/src/com/google/maps/android/utils/demo/ClusteringDemoActivity.java
+++ demo/src/com/google/maps/android/utils/demo/ClusteringDemoActivity.java
@@ -16,42 +16,167 @@
 
 package com.google.maps.android.utils.demo;
 
+import android.os.AsyncTask;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.maps.android.clustering.Cluster;
 import com.google.maps.android.clustering.ClusterManager;
+import com.google.maps.android.utils.demo.model.BaseDemoActivity;
 import com.google.maps.android.utils.demo.model.MyItem;
 
+import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Simple activity demonstrating ClusterManager.
- */
+
 public class ClusteringDemoActivity extends BaseDemoActivity {
     private ClusterManager<MyItem> mClusterManager;
+    private final static String mLogTag = "GeoJsonDemo";
+    private final String mGeoJsonUrl
+            = "https://data.sfgov.org/resource/ritf-b9ki.json";
+    // initialize time variable
+
+    double entry_time=0;
+    double thirty = 0;
+
+
+
 
     @Override
     protected void startDemo() {
-        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.503186, -0.126446), 10));
+        getMap().moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(37.773972, -122.431297), 10));
 
         mClusterManager = new ClusterManager<MyItem>(this, getMap());
         getMap().setOnCameraChangeListener(mClusterManager);
 
-        try {
-            readItems();
-        } catch (JSONException e) {
-            Toast.makeText(this, "Problem reading list of markers.", Toast.LENGTH_LONG).show();
-        }
+        DownloadGeoJsonFile downloadGeoJsonFile = new DownloadGeoJsonFile();
+        // Download the GeoJSON file
+        downloadGeoJsonFile.execute(mGeoJsonUrl);
+
+        Toast.makeText(this, "Zoom in to find data", Toast.LENGTH_LONG).show();
+
+
+
+
+
+
+
+
+
     }
 
-    private void readItems() throws JSONException {
-        InputStream inputStream = getResources().openRawResource(R.raw.radar_search);
-        List<MyItem> items = new MyItemReader().read(inputStream);
-        mClusterManager.addItems(items);
+
+
+    private class DownloadGeoJsonFile extends AsyncTask<String, Void, JSONObject> {
+
+        @Override
+        protected JSONObject doInBackground(String... params) {
+
+
+
+            try {
+                URL url = new URL(mGeoJsonUrl);
+                URLConnection conn = url.openConnection();
+                conn.setDoOutput(true);
+                conn.setRequestProperty("X-App-Token", "nOOgZHt8V7HN8W0HMJEV4fWgx");
+                // Open a stream from the URL
+                InputStream stream = new URL(params[0]).openStream();
+
+                String line;
+                StringBuilder result = new StringBuilder();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+                while ((line = reader.readLine()) != null) {
+                    // Read and save each line of the stream
+                    result.append(line);
+                }
+
+                // Close the stream
+                reader.close();
+                stream.close();
+
+
+                JSONArray array;
+
+                array = new JSONArray(result.toString());
+
+
+
+                // get number of milliseconds in 30 days ago
+                thirty = TimeUnit.MILLISECONDS.convert(-30, TimeUnit.DAYS);
+
+
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject obj = (JSONObject) array.get(i);
+                    String latitude = obj.optString("y").toString();
+                    String longitude = obj.optString("x").toString();
+                    String date = obj.optString("date").substring(0,10);
+
+                    double lat = Double.parseDouble(latitude);
+                    double longi = Double.parseDouble(longitude);
+
+                    DateFormat formatter ;
+                    Date date_format;
+
+                    formatter = new SimpleDateFormat("yyyy-mm-dd");
+                    try {
+                        date_format = formatter.parse(date);
+                        entry_time= date_format.getTime();
+
+                    } catch (ParseException e){
+                        e.printStackTrace();
+                    }
+
+
+
+
+                    MyItem offsetItem = new MyItem(lat,longi);
+                    if (entry_time > thirty) {
+                        mClusterManager.addItem(offsetItem);
+
+                    }
+
+
+
+                }
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                Log.e(mLogTag, "GeoJSON file could not be read");
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(JSONObject jsonObject) {
+
+        }
+
+
     }
+
+
+
+
+
+
 }
--- demo/src/com/google/maps/android/utils/demo/MainActivity.java
+++ demo/src/com/google/maps/android/utils/demo/MainActivity.java
@@ -21,7 +21,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 public class MainActivity extends Activity implements View.OnClickListener {
     private ViewGroup mListView;
@@ -31,37 +30,15 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.activity_main);
 
-        mListView = (ViewGroup) findViewById(R.id.list);
 
-        addDemo("Clustering", ClusteringDemoActivity.class);
-        addDemo("Clustering: Custom Look", CustomMarkerClusteringDemoActivity.class);
-        addDemo("Clustering: 2K markers", BigClusteringDemoActivity.class);
-        addDemo("PolyUtil.decode", PolyDecodeDemoActivity.class);
-        addDemo("PolyUtil.simplify", PolySimplifyDemoActivity.class);
-        addDemo("IconGenerator", IconGeneratorDemoActivity.class);
-        addDemo("SphericalUtil.computeDistanceBetween", DistanceDemoActivity.class);
-        addDemo("Generating tiles", TileProviderAndProjectionDemo.class);
-        addDemo("Heatmaps", HeatmapsDemoActivity.class);
-        addDemo("Heatmaps with Places API", HeatmapsPlacesDemoActivity.class);
-        addDemo("GeoJSON Layer", GeoJsonDemoActivity.class);
-        addDemo("KML Layer Overlay", KmlDemoActivity.class);
     }
 
-    private void addDemo(String demoName, Class<? extends Activity> activityClass) {
-        Button b = new Button(this);
-        ViewGroup.LayoutParams layoutParams = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-        b.setLayoutParams(layoutParams);
-        b.setText(demoName);
-        b.setTag(activityClass);
-        b.setOnClickListener(this);
-        mListView.addView(b);
-    }
 
     @Override
     public void onClick(View view) {
-        Class activityClass = (Class) view.getTag();
-        startActivity(new Intent(this, activityClass));
+        Intent myIntent = new Intent(MainActivity.this, ClusteringDemoActivity.class);
+        MainActivity.this.startActivity(myIntent);
     }
 }
